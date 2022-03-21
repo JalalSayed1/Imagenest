8aa0721399b41853c1076051c825216b1e0cbfa7
“Emma <2547542p@student.gla.ac.uk>
2022-03-21 14:12:15 +0000

    created upload view, model, form and HTML, not fully working

--- imagenest_project/imagenest/forms.py
+++ imagenest_project/imagenest/forms.py
@@ -6,6 +6,7 @@ from django.core.exceptions import ValidationError
 USERNAME_MAX_LENGTH = 15
 PASSWORD_MIN_LENGTH = 6
 PASSWORD_MAX_LENGTH = 40
+CAPTION_MAX_LENGTH = 100
 
 class LoginForm(forms.Form):
 
