b4f07487e8c3b7a4e5f6e5595a488c17c572b60f
Douglas Stewart <2555731s@student.gla.ac.uk>
2022-03-22 17:54:58 +0000

    user profiles mapped and dispalys users images, nothing more

--- imagenest_project/imagenest/models.py
+++ imagenest_project/imagenest/models.py
@@ -16,12 +16,6 @@ class Register(models.Model):
 
 
 class UserProfile(models.Model):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # website = models.URLField(blank=True)
-    # picture = models.ImageField(upload_to="profile_images", blank=True)
-
-    # def __str__(self):
-    #     return self.user.username
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     firstname = models.CharField(max_length=USERNAME_MAX_LENGTH, blank=True)
--- imagenest_project/imagenest/views.py
+++ imagenest_project/imagenest/views.py
@@ -77,26 +77,41 @@ def logout(request):
 
 
 @login_required
-def profile(request, user):
+def profile(request):
     # changed the urls so that their profile page will be at /profile/username
     # will need to change view so that it accesses user data through username parameter
-    
+    #username = UserProfile.objects.get(username=request.user)
     images = Image.objects.filter(username=request.user)
-
-    #line just here to remind me that profile page will almost definitely involve .filter() 
-    #images = Post.objects.filter(username=user)
-
-    # image1 = {"url":"https://source.unsplash.com/random?places", "username" :"username1", "likes" : 2, "likers" : ["usename11", "usename11"], 'id':4}
-    # images = {}
     
     profile_image = {"url":"https://source.unsplash.com/250x250?person", "username" :"username1", 'id' : 8}
+
+    context = {
+        "images" : images,
+        "profile_image" : profile_image
+        }
     
-    return render(request, "imagenest/profile.html", {"user": user, "profile_image" : profile_image, "images" : images})
+    return render(request, "imagenest/profile.html", context)
 
 
 @login_required
 def get_profile(request, user):
-    return render(request, "imagenest/get_profile.html", {"user": user})
+    try:
+        profile = UserProfile.objects.get(username=user)
+        user = profile.user
+        #profile_image = profile.profile_image
+        username = profile.username
+        images = Image.objects.all().filter(username=user)     
+    except UserProfile.DoesNotExist:
+        profile = None
+        images = None
+
+    context_dict = {
+        "images" : images,
+        "profile" : profile,
+        #"profile_image" : profile_image,
+        "username" : username
+        }
+    return render(request, "imagenest/profile.html", context_dict)
 
 
 @login_required
