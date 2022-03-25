ddf57f459b84951a9e857f71de72cc3bae74b674
2574266s <2574266s@student.gla.ac.uk>
2022-03-25 15:43:41 +0000

    Started search tests

--- imagenest_project/imagenest/tests.py
+++ imagenest_project/imagenest/tests.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
-from imagenest.forms import searchForm
+from django.urls import reverse, resolve
+from imagenest.forms import *
+import os
 from imagenest.models import UserProfile, Image, Like, User
 
 class SearchTests(TestCase):
-    ## test empty form
-    ## class MyTests(TestCase):
-    def test_search_form(self):
-        form_data = {'name': ''}
-        form = MyForm(data=form_data)
-        self.assertTrue(form.is_valid())
+
+    def test_search_form_exists(self):
+        import imagenest.forms
+        self.assertTrue('SearchForm' in dir(imagenest.forms), "The SearchForm class could not be found in forms.py")
+
+    def test_search_form_linked_to_User(self):
+        search_form = SearchForm()
+        self.assertEqual(type(search_form.__dict__['instance']), User, "The search form is not correctly linked to the User model.")
+    
+    def test_search_uses_template(self):
+        response = self.client.get(reverse('imagenest:search'))
+        self.assertTemplateUsed(response, 'imagenest/search.html', "The search.html template is not used for the search() view}")
