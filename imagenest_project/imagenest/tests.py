b5462a26ae8de6a1c7c90a9847990e0b45dc2765
“Emma <2547542p@student.gla.ac.uk>
2022-03-27 23:24:41 +0100

    all views tests complete

--- imagenest_project/imagenest/tests.py
+++ imagenest_project/imagenest/tests.py
@@ -10,16 +10,8 @@ class SearchTests(TestCase):
         search_form = SearchForm()
         self.assertEqual(type(search_form.__dict__['instance']), User, "The search form is not correctly linked to the User model.")
     
-    def test_search_uses_template(self):
-        response = self.client.get(reverse('imagenest:search'))
-        self.assertTemplateUsed(response, 'imagenest/search.html', "The search.html template is not used for the search() view}")
-        
 class uploadTests(TestCase):
     
-    def test_upload_uses_template(self):
-        response = self.client.get(reverse('imagenest:upload'))
-        self.assertTemplateUsed(response, 'imagenest/upload.html', "The upload.html template is not used for the add_picture() view}")
-        
     def test_upload(self):
         #how to test that a file/url has been uploaded?
         
@@ -49,4 +41,47 @@ class formsTests(TestCase):
     def test_search_form_exists(self):
         import imagenest.forms
         self.assertTrue('SearchForm' in dir(imagenest.forms), "The SearchForm class could not be found in forms.py")
+    
+class templateTests:    
+    #home.html
+    def test_home_uses_template(self):
+        response = self.client.get(reverse('imagenest:home'))
+        self.assertTemplateUsed(response, 'imagenest/home.html', "The home.html template is not used for the home() view}")
+    
+    
+    #login.html
+    def test_login_uses_template(self):
+        response = self.client.get(reverse('imagenest:login'))
+        self.assertTemplateUsed(response, 'imagenest/login.html', "The login.html template is not used for the login() view}")
+    
+    #logout.html
+    def test_logout_uses_template(self):
+        response = self.client.get(reverse('imagenest:logout'))
+        self.assertTemplateUsed(response, 'imagenest/logout.html', "The logout.html template is not used for the logout() view}")
+        
+    #profile.html
+    def test_profile_uses_template(self):
+        response = self.client.get(reverse('imagenest:profile'))
+        self.assertTemplateUsed(response, 'imagenest/profile.html', "The profile.html template is not used for the profile() view}")
+    
+    
+    #register.html
+    def test_register_uses_template(self):
+        response = self.client.get(reverse('imagenest:register'))
+        self.assertTemplateUsed(response, 'imagenest/register.html', "The register.html template is not used for the register() view}")
+    
+    #search.html
+    def test_search_uses_template(self):
+        response = self.client.get(reverse('imagenest:search'))
+        self.assertTemplateUsed(response, 'imagenest/search.html', "The search.html template is not used for the search() view}")
+    
+    #top_images.html
+    def test_top_images_uses_template(self):
+        response = self.client.get(reverse('imagenest:top_images'))
+        self.assertTemplateUsed(response, 'imagenest/top_images.html', "The top_images.html template is not used for the top_images() view}")
+        
+    #upload.html
+    def test_upload_uses_template(self):
+        response = self.client.get(reverse('imagenest:add_picture'))
+        self.assertTemplateUsed(response, 'imagenest/upload.html', "The upload.html template is not used for the add_picture() view}")
     
