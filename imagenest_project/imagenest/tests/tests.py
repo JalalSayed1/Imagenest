03d9f30d147c69493139cf08cd9d42f776ba2045
“HUIXIN <2470901z@student.gla.ac.uk>
2022-03-28 18:44:45 +0100

    first commit

--- imagenest_project/imagenest/tests/tests.py
+++ imagenest_project/imagenest/tests/tests.py
@@ -5,6 +5,7 @@ import os
 from imagenest.models import *
 from imagenest.views import *
 
+
 class SearchTests(TestCase):
 
     def test_search_form_linked_to_User(self):
@@ -141,3 +142,33 @@ class LoginTests(TestCase):
                    'password': ''}
         response = self.client.post(reverse(login), request)
         self.assertTemplateUsed(response, 'imagenest/login.html', "The login() view does not return the original form on error")
+
+class UploadTests(TestCase):
+
+    def setUp(self):
+        # Uploading requires an associated User, so we actually register and login
+        RegisterTests.test_register_success(self)
+        LoginTests.test_login_success(self)
+
+    def test_empty_upload_error(self):
+        request = {}
+        response = self.client.post(reverse(add_picture), request)
+
+        self.assertTemplateUsed(response, 'imagenest/upload.html', "The add_picture() view does not return the original form on error")
+
+    def test_url_upload_success(self):
+        request = {'image_url': 'https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg',
+                   'image_caption': 'Landscape'}
+        response = self.client.post(reverse(add_picture), request)
+
+        self.assertTrue(Image.objects.filter(url=request['image_url'], caption=request['image_caption']).exists(), "Added an image using URL but no Image object is created")
+        self.assertRedirects(response, reverse(home))
+
+    def test_url_upload_error(self):
+        request = {'image_url': 'https://www.google.com/',
+                   'image_caption': 'Google'}
+        response = self.client.post(reverse(add_picture), request)
+
+        self.assertTemplateUsed(response, 'imagenest/upload.html', "The add_picture() view does not return the original form on error")
+
+    
