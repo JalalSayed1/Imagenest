3c7d4042e184268acc6376c7ca3ab170f365708e
Jalal Sayed <mrjalalsayed@gmail.com>
2022-03-31 11:09:28 +0100

    Fixed population script errors with Image model

--- imagenest_project/populate_imagenest.py
+++ imagenest_project/populate_imagenest.py
@@ -39,42 +39,34 @@ def populate():
     # images to store on the database
     images = [
         {'url':'https://images.unsplash.com/photo-1624965085151-0710f6b3f284',
-         'likes':3,
          'username':'john1',
          'likers': ['ksmith', 'na1mi2', 'jdjd'],
          'caption': 'test'},
         {'url':'https://images.unsplash.com/photo-1572091574819-ea8bb5394b1d',
-         'likes':1,
          'username':'john1',
          'likers': ['jdjd'],
          'caption': 'test'},
         {'url':'https://images.unsplash.com/photo-1640622660721-45b83554ab05',
-         'likes':0,
          'username':'ksmith',
          'likers': [],
          'caption': 'test'},
         {'url':'https://images.unsplash.com/photo-1647471641611-908659d7b366',
-         'likes':5,
          'username':'ksmith',
          'likers': ['john1','liow123','jdjd','this_is_hu','na1mi2'],
          'caption': 'test'},
         {'url':'https://images.unsplash.com/photo-1647514422086-18cde746fa26',
-         'likes':2,
          'username':'john1',
          'likers': ['jdjd','this_is_hu'],
          'caption': 'test'},
         {'url':'https://images.unsplash.com/photo-1647363542902-9b76666d88b3',
-         'likes':3,
          'username':'this_is_hu',
          'likers': ['jdjd','this_is_hu', 'liow123'],
          'caption': 'test'},
         {'url':'https://images.unsplash.com/photo-1647482290110-df9a2895cb95',
-         'likes':4,
          'username':'ksmith',
          'likers': ['na1mi2','this_is_hu', 'liow123', 'jdjd'],
          'caption': 'test'},
         {'url':'https://images.unsplash.com/photo-1640622299541-8c8ab8a098f3',
-         'likes':1,
          'username':'liow123',
          'likers': ['this_is_hu'],
          'caption': 'test'}
@@ -86,7 +78,7 @@ def populate():
 
     # add each image to database
     for image in images:
-        add_image(image['url'], image['likes'], image['username'], image['likers'], caption['caption'])
+        add_image(image['url'], image['username'], image['likers'], image['caption'])
 
     # prints the image details being added to the database
     for user in User.objects.all():
@@ -114,18 +106,11 @@ def add_user_and_profile(firstname, surname, username, password):
     user.surname = surname
     user.password = password
     user.save()
-
-    # create UserProfile object using the parameters
-    #userProfile = UserProfile.objects.get_or_create(user=user, firstname=firstname, surname=surname, username=username)[0]
-    #userProfile.firstname = firstname
-    #userProfile.surname = surname
-    #userProfile.password = password
-    #userProfile.save()
-
+    
     return user
 
 
-def add_image(url, likes, username, likers, caption):
+def add_image(url, username, likers, caption):
     user = User.objects.get(username=username) # find associated User object
 
     try:
@@ -137,14 +122,13 @@ def add_image(url, likes, username, likers, caption):
 
     # set the image attributes
     image.url = url
-    image.likes = likes
     image.caption = caption
 
-    # set the users who liked the image and the likes
+    # set the users who liked the image
     for liker in likers:
         user_who_liked = User.objects.get(username=liker)
         image.likers.add(user_who_liked)
-        like = Like.objects.get_or_create(user=user_who_liked, image=image, value=LIKE_CHOICES[0])[0]
+        like = Like.objects.get_or_create(user=user_who_liked, image=image)[0]
         like.save()
 
     # save the image
